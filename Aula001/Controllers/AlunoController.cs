e6cfcb342d64d8f8205b1a83119220ec91c7c4a9
Ana <Ana@DESKTOP-0FGPOVA>
2024-08-07 10:08:36 -0300

    Add project files.

---
+++ Aula001/Controllers/AlunoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aula001.Controllers
+{
+    [ApiController]
+    [Route("api/aluno")]
+    public class AlunoController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Saudacao")]
+        public IActionResult Saudacao()
+        {
+            return Ok("Olá mundo");
+        }
+
+
+        [HttpGet]
+        [Route("SaudacaoNome")]
+        public IActionResult SaudacaoNome(string nome)
+        {
+            return Ok("$Olá mundo {nome}");
+        }
+    }
+}
