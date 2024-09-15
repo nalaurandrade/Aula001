a108f21a32152b37b8a3bc1bc5ec7a7df70424f2
Ana Laura Andrade <analaura140600@gmail.com>
2024-09-15 20:53:31 -0300

    questão4

--- Aula001/Controllers/AlunoController.cs
+++ Aula001/Controllers/AlunoController.cs
@@ -30,54 +30,7 @@ namespace Aula001.Controllers
             return Ok(listaAlunos);
         }
 
-        [HttpGet]
-        [Route("ObterPorCpf")]
-        public IActionResult ObterPorCpf(string cpfBusca)
-        {
-            var resultadoBusca = listaAlunos
-                                .Where(aluno => aluno.Cpf == cpfBusca)
-                                .FirstOrDefault();
-            if (resultadoBusca is null)
-                return NotFound($"O cpf {cpfBusca} não encontrado");
-                return Ok(resultadoBusca);
-        }
 
-        [HttpDelete]
-        [Route("Remover")]
-        public IActionResult Remover(string cpfBusca)
-        {
-            Aluno? resultadoBusca = listaAlunos
-                                .Where(aluno => aluno.Cpf == cpfBusca)
-                                .FirstOrDefault();
-
-            if (resultadoBusca is null)
-                return NotFound($"O cpf {cpfBusca} não encontrado");
-
-            listaAlunos.Remove(resultadoBusca);
-
-            return Ok("Aluno removido com sucesso!");
-        }
-
-        [HttpGet]
-        [Route("Atualizar/{cpfBusca}")]
-        public IActionResult Atualizar(string cpfBusca, 
-                                                        Aluno alunoAtualizado)
-        {
-            Aluno? resultadoBusca = listaAlunos
-                                .Where(aluno => aluno.Cpf == cpfBusca)
-                                .FirstOrDefault();
-
-            if (resultadoBusca is null)
-                return NotFound($"O cpf {cpfBusca} não encontrado");
-
-
-            resultadoBusca.Nome = alunoAtualizado.Nome;
-            resultadoBusca.Idade = alunoAtualizado.Idade;
-
-            return Ok("Atualizado");
-
-
-        }
 
         /*[HttpGet]
         [Route("SaudacaoNome")]
--- Aula001/Models/Aluno.cs
+++ Aula001/Models/Aluno.cs
@@ -5,27 +5,23 @@ namespace Aula001.Models
 {
     public class Aluno
     {
-
-        [Required(ErrorMessage = "Cpf é obrigatório")]
-        [MinLength(11, ErrorMessage = "Cpf com no mínimo de 11 caracteres")]
-        [MaxLength(11, ErrorMessage = "Cpf com no máximo de 11 caracteres")]
-        public string Cpf { get; set; }
-
-
-
         [Required(ErrorMessage = "Nome é obrigatório")]
         [MinLength(3, ErrorMessage = "Nome com mínimo de 3 caracteres")]
         [MaxLength(50, ErrorMessage = "Nome com máximo de 50 caracteres")]
         public string Nome { get; set; }
 
+        [Required(ErrorMessage = "Nome é obrigatório")]
+        [MinLength(2, ErrorMessage = "Nome deve conter mais de 2 caracteres")]
 
+        public string RA { get; set; }
 
-        [Required(ErrorMessage = "Idade é obrigatória")]
-        /*[Range(18, 80, ErrorMessage = "Intervalo de idade deve ser entre 18 e 20")]*/
-        [MaioridadeValidation(
-            ErrorMessage = "Idade deve ser maior ou igua 18")]
-        public int Idade { get; set; }
+        [Required(ErrorMessage = "RA é obrigatório")]
+        [MinLength(5, ErrorMessage = "Nome com 6 caracteres")]
+        [MaxLength(6, ErrorMessage = "Nome com 6 caracteres")]
+        public string email { get; set; }
 
+        [MinLength(5, ErrorMessage = "email com 6 caracteres")]
+        [MaxLength(6, ErrorMessage = "email com 6 caracteres")]
         public string MyProperty { get; set; }
         [Compare(nameof(MyProperty), ErrorMessage = "Campos estao diferentes")]
         public string MyProperty2 { get; set; }
